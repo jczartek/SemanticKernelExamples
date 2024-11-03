f588c80fa835c6338b10eb191c692ec4a9c2ef1b
Jakub Czartek <kuba@linux.pl>
2024-11-03 11:27:18 +0100

    Add new YAML-based translation project
    Introduced a new project `FileYamlPropmts` that includes configuration for .NET Sdk, necessary NuGet packages, and a sample translation program using OpenAI. Added a new YAML prompt file for translation functionality. Updated the solution file to include the new project.

---
+++ FileYamlPropmts/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var builder = new ConfigurationBuilder()
+    .AddUserSecrets<Program>();
+
+var configuration = builder.Build();
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion("gpt-4o-mini", configuration["apiKey"]!)
+    .Build();
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "..","..","..", "Prompts", "Translate.yaml");
+var translateFunc = kernel.CreateFunctionFromPromptYaml(path);
+
+var result = await kernel.InvokeAsync(translateFunc, new KernelArguments
+{
+    ["source_language"] = "polish",
+    ["target_language"] = "english",
+    ["text_to_translate"] = "Pracuj tak ciężko jak to możliwe, to zwiększa szanse na sukces. Jeśli inni ludzie pracują 40 godzin w tygodniu, a ty 100 godzin, to ty w 4 miesiące osiągniesz to, co innym zajmie rok."
+}) ;
+
+Console.WriteLine(result);
+Console.ReadLine();
